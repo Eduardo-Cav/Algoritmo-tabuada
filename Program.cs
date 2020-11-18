39c6fcad763579a350788d7d174e8346be4b103c
Eduardo <eduzika2903@hotmail.com>
2020-11-17 22:02:36 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algoritmo_tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a o valor da tabuada que deseja saber: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int z = 0; z < 11; z++){
+            Console.WriteLine($"{x} x {z} = {x * z}");
+
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
